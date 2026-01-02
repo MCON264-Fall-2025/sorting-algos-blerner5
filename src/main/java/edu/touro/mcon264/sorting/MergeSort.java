56654d04e31bb5cd72df36cb6839d24d9f0c14ad
blerner5 <blerner5@student.touro.edu>
2026-01-02 01:03:56 -0500

    implemented the sorting algorithms
    implemented methods

--- src/main/java/edu/touro/mcon264/sorting/MergeSort.java
+++ src/main/java/edu/touro/mcon264/sorting/MergeSort.java
@@ -6,6 +6,39 @@ public class MergeSort implements Sorter {
 
     @Override
     public <T> void sort(T[] a, Comparator<? super T> comp) {
-        // TODO: implement merge sort
+        if (a == null || a.length <= 1) {
+            return;
+        }
+        @SuppressWarnings("unchecked") T[] aux = (T[]) new Object[a.length];
+        mergeSort(a, aux, 0, a.length - 1, comp);
+    }
+
+    private <T> void mergeSort(T[] a, T[] aux, int left, int right, Comparator<? super T> comp) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        mergeSort(a, aux, left, mid, comp);
+        mergeSort(a, aux, mid + 1, right, comp);
+        merge(a, aux, left, mid, right, comp);
+    }
+
+    private <T> void merge(T[] a, T[] aux, int left, int mid, int right, Comparator<? super T> comp) {
+        for (int i = left; i <= right; i++) {
+            aux[i] = a[i];
+        }
+        int i = left;
+        int j = mid + 1;
+        int k = left;
+        while (i <= mid && j <= right) {
+            if (comp.compare(aux[i], aux[j]) <= 0) {
+                a[k++] = aux[i++];
+            } else {
+                a[k++] = aux[j++];
+            }
+        }
+        while (i <= mid) {
+            a[k++] = aux[i++];
+        }
     }
 }
